0c77b4d8905f0c7736af07ea9f9e1717a1bce181
kilian <kilian.buehling@googlemail.com>
2022-06-02 19:50:56 +0200

    test2

---
+++ tests/test_get_4chan_post.R
@@ -0,0 +1,5 @@
+
+test_that("Always gets same posts", {
+  expect_equal(nchar(get_4chan_post(board = "trv", post = 2226503)$comments), 245)
+  expect_error(get_4chan_post(board = "trv", post = 0))
+})
--- tests/testthat.R
+++ tests/testthat.R
@@ -9,11 +9,4 @@
 library(testthat)
 library(fouRplebsAPI)
 
-
-test_that("Always gets same posts", {
-  expect_equal(nchar(get_4chan_post(board = "trv", post = 2226503)$comments), 245)
-  expect_error(get_4chan_post(board = "trv", post = 0))
-})
-
-
 test_check("fouRplebsAPI")
