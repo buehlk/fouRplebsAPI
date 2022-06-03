cf01d1c367fb3fa1838d3a9008d9edfb922929ea
kilian <kilian.buehling@googlemail.com>
2022-06-03 11:01:34 +0200

    list test

--- tests/testthat/test_search_4chan_snippet.R
+++ tests/testthat/test_search_4chan_snippet.R
@@ -6,5 +6,5 @@ test_that("Always get the 25 most recent posts", {
 })
 
 test_that("search_4chan_snippet() works", {
-  testthat::expect_type(search_4chan_snippet(boards = "adv", text = "kitties", show_only = "image", result_type = "results_num", cool = 20)["total_found"], "integer")
+  testthat::expect_type(search_4chan_snippet(boards = "adv", text = "kitties", show_only = "image", result_type = "results_num", cool = 20)["total_found"], "list")
 })
