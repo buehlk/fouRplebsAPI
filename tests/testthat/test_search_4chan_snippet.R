dad66cb28bc186405ff4c88bbcdd62fd6a3262af
kilian <kilian.buehling@googlemail.com>
2022-06-03 11:00:56 +0200

    test cooldown

--- tests/testthat/test_search_4chan_snippet.R
+++ tests/testthat/test_search_4chan_snippet.R
@@ -1,10 +1,10 @@
 test_that("Always get the 25 most recent posts", {
-  expect_equal(nrow(search_4chan_snippet(boards = "adv", text = "kitties", show_only = "image")),
+  expect_equal(nrow(search_4chan_snippet(boards = "adv", text = "kitties", show_only = "image", cool = 20)),
                25)
 
-  expect_error(search_4chan_snippet(boards = "adv", text = "kitties", show_only = "videos"))
+  expect_error(search_4chan_snippet(boards = "adv", text = "kitties", show_only = "videos", cool = 20))
 })
 
 test_that("search_4chan_snippet() works", {
-  testthat::expect_type(search_4chan_snippet(boards = "adv", text = "kitties", show_only = "image", result_type = "results_num")["total_found"], "integer")
+  testthat::expect_type(search_4chan_snippet(boards = "adv", text = "kitties", show_only = "image", result_type = "results_num", cool = 20)["total_found"], "integer")
 })
