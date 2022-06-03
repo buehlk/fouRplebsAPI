833153d77177659f87307e1be15b1ccc533cb818
kilian <kilian.buehling@googlemail.com>
2022-06-03 12:37:44 +0200

    line length in tests

--- tests/testthat/test_get_4chan_boards_index.R
+++ tests/testthat/test_get_4chan_boards_index.R
@@ -1,9 +1,11 @@
 test_that("Always get the 10 most recent threads", {
-  expect_equal(nrow(get_4chan_board_index(board = "trv", page = 1, latest_comments = FALSE)),
+  expect_equal(nrow(get_4chan_board_index(board = "trv", page = 1,
+                                          latest_comments = FALSE)),
                10)
   expect_error(get_4chan_post(board = "trv", page = 0))
 })
 
 test_that("get_4chan_post() works", {
-  testthat::expect_type(get_4chan_board_index(board = "trv", page = 1, latest_comments = FALSE), "list")
+  testthat::expect_type(get_4chan_board_index(board = "trv", page = 1,
+                                              latest_comments = FALSE), "list")
 })
--- tests/testthat/test_search_4chan_snippet.R
+++ tests/testthat/test_search_4chan_snippet.R
@@ -1,8 +1,12 @@
 test_that("Always get the 25 most recent posts", {
-  expect_equal(nrow(search_4chan_snippet(boards = "adv", text = "kitties", show_only = "image", cool = 20)),
+  expect_equal(nrow(search_4chan_snippet(boards = "adv", text = "kitties",
+                                         show_only = "image", cool = 20)),
                67)
 })
 
 test_that("search_4chan_snippet() works", {
-  testthat::expect_type(search_4chan_snippet(boards = "adv", text = "kitties", show_only = "image", result_type = "results_num", cool = 20)["total_found"], "list")
+  testthat::expect_type(search_4chan_snippet(boards = "adv", text = "kitties",
+                                             show_only = "image",
+                                             result_type = "results_num",
+                                             cool = 20)["total_found"], "list")
 })
